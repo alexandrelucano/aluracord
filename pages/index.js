cf28f59eaf3f104a2a1c6a32bee7ea2a98c751bf
Alexandre Lucano <lucano@embras.net>
2022-01-26 10:52:27 -0300

    feat: Estilo global

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -63,15 +37,15 @@ function Titulo(props) {
 export default function PaginaInicial() {
   // const username = 'alexandrelucano';
   const [username, setUsername] = React.useState('alexandrelucano');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: '#e1ebf5',
-          backgroundImage: 'url(https://img.wallpapersafari.com/desktop/1366/768/99/26/uhf6Ek.jpg)',
+          backgroundImage: appConfig.theme.background,
           // backgroundImage: 'radial-gradient(at 47% 33%, hsl(206.67, 43%, 92%) 0, transparent 59%), radial-gradient(at 82% 65%, hsl(88.65, 73%, 48%) 0, transparent 55%)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
@@ -96,7 +70,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
-            onSubmit={(event) => event.preventDefault()}
+            onSubmit={(event) => {
+              event.preventDefault()
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -110,7 +87,10 @@ export default function PaginaInicial() {
             <TextField
               fullWidth
               value={username}
-              onChange={(event)=> setUsername(event.target.value)}
+              onChange={(event)=> {
+                const userValue = event.target.value;
+                setUsername(event.target.value)
+              }}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
